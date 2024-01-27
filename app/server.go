a55c1e045a4f5156fba8ea7c668aef2f921d1c49
SerhioTis <olegdaschuk@gmail.com>
2024-01-27 02:16:35 +0200

    Respond with 404

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,16 +13,36 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
+	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			continue
+		}
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Writing to resp: ", err.Error())
-		os.Exit(1)
+		buffer := make([]byte, 1024)
+		if _, err := conn.Read(buffer); err != nil {
+			fmt.Println("Error reading from connection: ", err.Error())
+			continue
+		}
+
+		resp := strings.Split(string(buffer), "\r\n")
+
+		httpInfo := strings.Split(resp[0], " ")
+
+		if httpInfo[1] == "/" {
+			if _, err = conn.Write([]byte("HTTP/1.1 200 Ok\r\n\r\n")); err != nil {
+				fmt.Println("Writing to resp: ", err.Error())
+				continue
+			}
+		} else {
+			if _, err = conn.Write([]byte("HTTP/1.1 400 Not Found\r\n\r\n")); err != nil {
+				fmt.Println("Writing to resp: ", err.Error())
+				continue
+			}
+		}
+		conn.Close()
 	}
 }
